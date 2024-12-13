c98db8196242ce5274f0bfdd228c8b1c3254aca4
yongjxu <26626454+yongjxu@users.noreply.github.com>
2024-12-13 14:17:32 +0800

    Update main.tsx
    添加 @twa-dev/sdk

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import WebApp from '@twa-dev/sdk'
 import App from './App.tsx'
+
+WebApp.ready();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
